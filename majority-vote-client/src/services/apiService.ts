8c67f58fc6a308d8e9e32c18be9d837006f51ca9
Marshall Scott <marshall@rubyplaynetwork.com>
2024-11-01 23:55:27 +1300

    feat: Hook up client to backend

--- majority-vote-client/src/services/apiService.ts
+++ majority-vote-client/src/services/apiService.ts
@@ -4,24 +4,13 @@ import axios from "axios"
 axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 export async function getQuestion(): Promise<QuestionType> {
-  // const response = await axios.get('/question')
+  const response = await axios.get('/question')
 
-  // return response.data
-
-  return {
-    question: 'Do ants have lungs',
-    leftOption: 'Yes',
-    rightOption: 'No',
-    questionId: '1'
-  }
+  return response.data
 }
 
 export async function submitAnswer(questionId: string, answer: QuestionAnswer): Promise<QuestionResult> {
-  // const response = await axios.push(`/question/${questionId}/answer`, { answer })
+  const response = await axios.post(`/question/${questionId}/answer`, { answer })
 
-  // return response.data
-
-  return {
-    percentage: 0.3
-  }
+  return response.data
 }
