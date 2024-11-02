9418445965f6581c9f20de44687a581fa63f12fd
Marshall Scott <marshall@rubyplaynetwork.com>
2024-11-02 15:55:10 +1300

    feat: Delete server

--- majority-vote-server/src/routes/root.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyPluginAsync } from 'fastify'
-import { questionRouter } from './question'
-
-const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.register(questionRouter, { prefix: '/question' })
-}
-
-export default root;
--- majority-vote-server/src/types/getQuestion.ts
+++
@@ -1,9 +0,0 @@
-import { Type, type Static } from "@fastify/type-provider-typebox";
-
-export const GetQuestionResponse = Type.Optional(Type.Object({
-    id: Type.String(),
-    statement: Type.String(),
-    leftOption: Type.String(),
-    rightOption: Type.String()
-}))
-export type GetQuestionResponseType = Static<typeof GetQuestionResponse>
